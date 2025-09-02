6cea923c3ab8dc88faa695c410a0dc6589f342f7
Pasindu Jayawardhane <108786553+Pazeezz@users.noreply.github.com>
2025-09-02 15:05:09 +0530

    Updated the PyData

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,4 +5,4 @@ c = TestClient(app)
 def test_root():
     r = c.get("/")
     assert r.status_code == 200
-    assert r.json() == {"message": "Hello, Pydata UOK students !"}
+    assert r.json() == {"message": "Hello, PyData UOK students !"}
